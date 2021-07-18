4fdc0d60d46eee70cbae3e65496375bad061ec35
Zdzislaw <zkrysztofiak@gmail.com>
2021-07-18 23:01:43 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+	<Button type="primary">
+		Guxiczek
+	</Button>
+	<button
+          type="button"
+      //     type="button"
+      //     onClick={() => send("SUBMIT")}
+      //     disabled={!canSubmit}
+          disabled={false}
+      //     data-state={state.toStrings().join(" ")}
         >
-          Learn React
-        </a>
-      </header>
+          {/* {state.matches("editing") && "Submit"}
+          {state.matches("submitted") && "Success!"} */}
+	    Abrakadabra
+        </button>
     </div>
   );
 }
